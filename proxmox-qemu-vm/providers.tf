be5c7d2f7ebaa57d9f7d2c20d4bd9633f786afdf
Matthew Topping <matthewatopping@gmail.com>
2024-03-28 16:44:27 +1300

    Remove Telmate provider as it is busted

--- proxmox-qemu-vm/providers.tf
+++ proxmox-qemu-vm/providers.tf
@@ -2,9 +2,13 @@ terraform {
   required_version = ">= 0.13"
   required_providers {
     proxmox = {
-      source  = "Telmate/proxmox"
-      version = "3.0.1-rc1"
+      source  = "TheGameProfi/proxmox"
+      version = "2.9.16"
     }
+    # proxmox = {
+    #   source  = "Telmate/proxmox"
+    #   version = "3.0.1-rc1"
+    # }
     cloudflare = {
       source  = "cloudflare/cloudflare"
       version = "~> 4.0"
